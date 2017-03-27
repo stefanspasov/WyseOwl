c34a7f6bd5c71b96b816aed7b49077127e84975f
Stefan Spasov <stespa@jetshop.se>
2017-03-27 18:19:37 +0200

    New project

---
+++ WyseOwl/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+
+namespace WyseOwl.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using EdgeJs;
+
+    using WyseOwl.Logic;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            try
+            {
+                var func = Edge.Func(@"
+                return function (data, callback) {
+                callback(null, 'Node.js welcomes ' + data);
+            }");
+                var a = func(".NET").Result;
+                return this.View();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetBaseException().Message);
+                return this.View();
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WyseOwl/Models/CalculationsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WyseOwl.Models
+{
+    public class CalculationsViewModel1
+    {
+        public class FirstCalculation
+        {
+            [Required]
+            [Display(Name = "Balance")]
+            public string Balance { get; set; }
+
+            [Required]
+            [Display(Name = "Home Address")]
+            public string HomeAddress { get; set; }
+        }
+    }
+}
