9840a53c3f6518a1b066038779c640fff4b9f992
Stefan Spasov <stespa@jetshop.se>
2017-03-30 19:23:34 +0200

    Cleanup

--- WyseOwl/Controllers/HomeController.cs
+++ WyseOwl/Controllers/HomeController.cs
@@ -2,30 +2,12 @@
 
 namespace WyseOwl.Controllers
 {
-    using System;
-    using System.Threading.Tasks;
-
-    using EdgeJs;
-
-    using WyseOwl.Logic;
-
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            try
             {
-                var func = Edge.Func(@"
-                return function (data, callback) {
-                callback(null, 'Node.js welcomes ' + data);
-            }");
-                var a = func(".NET").Result;
-                return this.View();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.GetBaseException().Message);
-                return this.View();
+                return View();
             }
         }
 
